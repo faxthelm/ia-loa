3f3bd0e47994efd0d55009d47eed8a0e6b56ba5b
BrunoIshiyama <bruno.yuji.ishi@gmail.com>
2019-10-29 16:18:20 -0300

    Initial features of problem manager

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -3,9 +3,9 @@ package application;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.Map;
 
 import utils.Parser;
+import utils.ProblemManager;
 
 /**
  * Hello world!
@@ -16,14 +16,12 @@ public class App
 	
     public static void main( String[] args ) throws InterruptedException
     {
-        Path p = new File("src/main/resources/DeterministicGoalState/navigation_10.net").toPath();
+        Path p = new File("src/main/resources/DeterministicGoalState/navigation_1.net").toPath();
         Parser parser = new Parser();
         try {
-			Map<String, Object> re = parser.readFile(p);
-			for(String key : re.keySet()) {
-				System.out.println(key);
-				System.out.println(re.get(key)!= null);
-			}
+			ProblemManager.init(parser.readFile(p));
+			System.out.println(ProblemManager.getApplicableActions(ProblemManager.getInitialState()));
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
