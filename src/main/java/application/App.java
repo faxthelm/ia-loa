d58120a0175cb9a9b55b822b31dba7c0864b49c0
BrunoIshiyama <bruno.yuji.ishi@gmail.com>
2019-10-28 22:32:28 -0300

    file reader implemented

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -1,5 +1,12 @@
 package application;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Map;
+
+import utils.Parser;
+
 /**
  * Hello world!
  *
@@ -9,6 +16,15 @@ public class App
 	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Path p = new File("src/main/resources/DeterministicGoalState/navigation_1.net").toPath();
+        Parser parser = new Parser();
+        try {
+			Map<String, Object> re = parser.readFile(p);
+			System.out.println(re);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
     }
 }
